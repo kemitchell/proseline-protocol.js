dd1586a2e285b99a55dc41a9f2ab0c78fcc9a6d5
Kyle E. Mitchell <kyle@kemitchell.com>
2018-07-20 17:47:28 -0700

    WIP

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var encryptedJSONProtocol = require('encrypted-json-protocol')
-var verifySignature = require('./verify-signature')
 var strictObjectSchema = require('strict-json-object-schema')
+var verifySignature = require('./verify-signature')
 
 var logEntrySchema = strictObjectSchema({
   publicKey: {
@@ -16,29 +16,63 @@ var logEntrySchema = strictObjectSchema({
 
 var envelopeSchema = strictObjectSchema({
   message: strictObjectSchema({
-    project: {
-      title: 'project discovery key',
-      type: 'string',
-      pattern: '^[a-f0-9]{64}$'
+    authorization: {
+      title: 'signature with project write key',
+      pattern: '^[a-f0-9]{128}$'
     },
-    index: {
-      title: 'log entry index',
-      type: 'integer',
-      minimum: 0
-    },
-    body: {
-      title: 'log entry payload',
-      type: 'object'
+    entry: {
+      type: 'object',
+      properties: {
+        project: {
+          title: 'project discovery key',
+          type: 'string',
+          pattern: '^[a-f0-9]{64}$'
+        },
+        publicKey: {
+          title: 'log public key',
+          type: 'string',
+          pattern: '^[a-f0-9]{64}$'
+        },
+        index: {
+          title: 'log entry index',
+          type: 'integer',
+          minimum: 0,
+          multipleOf: 1
+        },
+        hash: {
+          title: 'SHA256 hash of the unencrypted payload',
+          type: 'string',
+          pattern: '^[a-f0-9]{64}$'
+        },
+        prior: {
+          title: 'SHA256 hash of the prior message payload',
+          comment: 'not required',
+          type: 'string',
+          pattern: '^[a-f0-9]{64}$'
+        },
+        encrypted: {
+          title: 'log entry payload',
+          type: 'string',
+          contentEncoding: 'base64',
+          pattern: '^(?:[A-Za-z0-9+\\/]{4})*(?:[A-Za-z0-9+\\/]{2}==|[A-Za-z0-9+\\/]{3}=)$'
+        },
+        signature: {
+          title: 'signature with log secret key',
+          pattern: '^[a-f0-9]{128}$'
+        }
+      },
+      required: [
+        'project',
+        'publicKey',
+        'index',
+        'hash',
+        // missing: prior
+        'encrypted',
+        'signature'
+      ],
+      additionalProperties: false
     }
-  }),
-  publicKey: {
-    type: 'string',
-    pattern: '^[a-f0-9]{64}$'
-  },
-  signature: {
-    type: 'string',
-    pattern: '^[a-f0-9]{128}$'
-  }
+  })
 })
 
 module.exports = {
